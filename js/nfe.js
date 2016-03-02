97475048b855ed87038eaefa4ec4af5a195d6583
Joshua Jenkins <josh@joshuajenkins.com>
2016-03-02 10:34:04 -0800

    Add meter element

--- js/nfe.js
+++ js/nfe.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
 
-  var progressPosition = 50;
-
-  var progressProgress=setInterval(function(){
-    progressTimer()
-  }, 1000);
+  var progressValue = 50;
+  var progressProgress=setInterval(progressTimer, 1000);
 
   function progressTimer() {
-    $('.progress-element progress').attr('value', progressPosition);
-    $('.progress-progress').text(progressPosition);
+    $('.progress-element progress').attr('value', progressValue);
+    $('.progress-progress').text(progressValue);
 
-    if (progressPosition >= 100) {
-      progressPosition = 0;
+    if (progressValue >= 100) {
+      progressValue = 0;
     } else {
-      progressPosition += 2;
+      progressValue += 2;
+    }
+  }
+
+  var meterValue = 50;
+  var meterProgress = setInterval(meterTimer, 250);
+
+  function meterTimer() {
+    $('.meter-element meter').attr('value', meterValue);
+    $('.meter-value').text(meterValue);
+
+    if (meterValue >= 100) {
+      meterValue = 0;
+    } else {
+      meterValue += 2;
     }
   }
 
