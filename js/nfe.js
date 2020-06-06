914bb3abb6d16a8aa831c9a8a923d467eb177590
Joshua Jenkins <josh@joshuajenkins.com>
2020-06-06 14:32:58 -0700

    Spring cleaning

--- js/nfe.js
+++ js/nfe.js
@@ -1,31 +1,38 @@
-$(document).ready(function() {
+let progressValue = 50
+const progressProgress = setInterval(progressTimer, 1000)
 
-  var progressValue = 50;
-  var progressProgress=setInterval(progressTimer, 1000);
+const progressEl = document.getElementById('progress-meter')
+const progressTextEl = document.getElementById('progress-meter-value')
 
-  function progressTimer() {
-    $('.progress-element progress').attr('value', progressValue);
-    $('.progress-progress').text(progressValue);
+function progressTimer() {
+	progressEl.setAttribute("value", progressValue)
+	progressTextEl.textContent = `${progressValue}`
 
-    if (progressValue >= 100) {
-      progressValue = 0;
-    } else {
-      progressValue += 2;
-    }
-  }
+	progressValue = (progressValue >= 100) ? 0 : progressValue += 2
+}
 
-  var meterValue = 50;
-  var meterProgress = setInterval(meterTimer, 250);
+let meterValue = 50
+const meterProgress = setInterval(meterTimer, 250)
 
-  function meterTimer() {
-    $('.meter-element meter').attr('value', meterValue);
-    $('.meter-value').text(meterValue);
+const meterEl = document.getElementById('meter-meter')
+const meterTextEl = document.getElementById('meter-meter-value')
 
-    if (meterValue >= 100) {
-      meterValue = 0;
-    } else {
-      meterValue += 2;
-    }
-  }
+function meterTimer() {
+	meterEl.setAttribute("value", meterValue)
+	meterTextEl.textContent = `${meterValue}`
 
-});
+	meterValue = (meterValue >= 100) ? 0 : meterValue += 2
+}
+
+let showDisabled = false
+const showDisabledEl = document.getElementById('show-disabled')
+const elementsEl = document.getElementById('elements')
+
+showDisabledEl.onclick = () => {
+	showDisabled = !showDisabled
+	if (showDisabled) {
+		elementsEl.classList.add('disabled-visible')
+	} else {
+		elementsEl.classList.remove('disabled-visible')
+	}
+}
